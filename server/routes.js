a9b3ab6cf7c988f53ff9863fb86d0c95e583de3f
Kim Maida <kim.maida@auth0.com>
2018-04-08 13:47:47 -0400

    Add a POST endpoint for favoriting a dinosaur

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,10 @@
 // Data
-const dinosaurs = require('./data/dinosaurs.json');
+const source = require('./data/dinosaurs.json');
+let dinos = [...source];
+
+function randomDelay() {
+  return Math.random() * 3000;
+}
 
 module.exports = function(app) {
   // API works (public)
@@ -9,13 +14,26 @@ module.exports = function(app) {
 
   // GET dinosaurs data
   app.get('/api/dinosaurs', (req, res) => {
-    res.send(dinosaurs);
+    res.send(dinos);
   });
 
   // GET dinosaurs data (delayed to simulate non-local environment)
   app.get('/api/delay/dinosaurs', (req, res) => {
     setTimeout(() => {
-      res.send(dinosaurs);
-    }, Math.random() * 3000);
+      res.send(dinos);
+    }, randomDelay());
+  });
+
+  // POST mark dino as a favorite
+  app.post('/api/fav', (req, res) => {
+    const dinoName = req.body.name;
+    const matchingDino = dinos.filter(d => d.name === dinoName)[0];
+
+    if (!matchingDino) {
+      res.status(404).send({error: `Cannot find a dinosaur called "${dinoName}"`});
+    } else {
+      matchingDino.favorite = true;
+      res.send(matchingDino);
+    }
   });
 };
